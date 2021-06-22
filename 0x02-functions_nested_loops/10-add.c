460af3ee48a974b0057d42c271ad5e540b43a8a7
Benson King'ori <bensonkingori96@gmail.com>
2021-06-22 07:11:48 -0700

    f

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- *add - adds two numbers x and y
- *return: return (x + y)
- *@x- The first number
- *@y- The second number
+ * add - adds two numbers x and y
+ * return: return (x + y)
+ * x- The first number
+ * y- The second number
   */
 
 int add(int x, int y)
-{ 
+{
 return (x + y);
 }
