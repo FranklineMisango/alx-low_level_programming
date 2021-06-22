84b863fb3cbe8fc6475f61a6290cc4f9df6b2f88
Benson King'ori <bensonkingori96@gmail.com>
2021-06-22 10:29:56 -0700

    r

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,15 @@
 
 /**
  * add - adds two numbers x and y
- * return: return (x + y)
+ * return: integer number
  * @x- int type number
  * @y- int type number
   */
 
 int add(int x, int y)
 {
-return (x + y);
+  int x;
+  int y;
+  int a;
+return (a);
 }
