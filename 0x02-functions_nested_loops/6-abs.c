a811dc324ed1123f7e6e272486176c1276d06604
Benson King'ori <bensonkingori96@gmail.com>
2021-06-22 12:35:30 -0700

    g

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
 /**
- * _abs: returns absolute value
+ * _abs - returns absolute value
  *_putchar: Prints character
  *@n: integer entered
- *Return: absolute value
+ *Return: 0 or 1
  *Description: returns absolute value
  */
 
 int _abs(int n)
 {
+
 return (n >= 0 ? n : n * -1);
 }
