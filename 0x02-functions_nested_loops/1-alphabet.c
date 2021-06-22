5d784efbd47d1b94e1641c0cfca379fb03a8f120
Benson King'ori <bensonkingori96@gmail.com>
2021-06-22 01:20:18 -0700

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *main- Entry point
+ *_putchar: Prints character
+ *return: 0
+ */
+
+int main(void)
+{
+char x;
+x = 'a';
+while (x <= 'z')
+{
+_putchar(x);
+x++;
+}
+_putchar(x)
+return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,1 +1,4 @@
+/*Function prototype file*/
+
+/*0-print holberton then return a new line*/
 char _putchar(char x);
