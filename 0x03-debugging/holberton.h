78620021f969fdabf114be3fdf816671abf1e86c
Benson King'ori <bensonkingori96@gmail.com>
2021-06-23 02:01:54 -0700

    r

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,11 @@
+/*Signage of a number*/
+void positive_or_negative(int i);
+
+/*Compare 3 numbers and returns the largest*/
+int largest_number(int a, int b, int c);
+
+/*Converts day of the month into day of the year*/
+int convert_day(int month,int day);
+
+/*Prints remaining days in a year*/
+void print_remaining_days(int month, int day, int year);
