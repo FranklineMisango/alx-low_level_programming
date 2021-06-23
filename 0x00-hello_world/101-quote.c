f30b9caf1fb7c4be51aa3c18beea7eeaeef7a76c
Benson King'ori <bensonkingori96@gmail.com>
2021-06-23 09:39:45 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,8 @@ int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, ch, sizeof(ch));
+ fprintf(stderr, RED "[ERROR]"
+               NC  ": No string argument provided! \n"
+                   "You must provide a program path as argument\n");
 return (1);
 }
