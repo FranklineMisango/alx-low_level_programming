43fd032767f587179991849a865f06b00ff96f3c
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-06-24 20:57:37 +0400

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,18 +1,29 @@
 #include "holberton.h"
 
 /**
- * mul - multiplies 2 integers
- * @a: first number
- * @b: second number
- * Description: multiplies and b
- * Return: result
+ * print_square - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
 
-int mul(int a, int b)
+void print_square(int n)
 {
-int x;
 
-x = a * b;
+	int i = 0, ii;
 
-return (x);
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
 }
