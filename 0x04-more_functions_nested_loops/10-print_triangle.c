454032e72dca5365c8040dd6b60e57113861503c
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-06-24 20:59:16 +0400

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,18 +1,34 @@
 #include "holberton.h"
 
 /**
- * mul - multiplies 2 integers
- * @a: first number
- * @b: second number
- * Description: multiplies and b
- * Return: result
+ * print_triangle - check for a digit
+ * @size : integer type
+ * Return:void
  */
 
-int mul(int a, int b)
+void print_triangle(int size)
 {
-int x;
 
-x = a * b;
+	int i = 1, ii;
 
-return (x);
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
 }
