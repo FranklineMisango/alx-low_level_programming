a0ded906778307b83921369e41f78716638d6d22
Benson King'ori <bensonkingori96@gmail.com>
2021-06-24 10:48:10 -0700

    f

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * _is digit - checks if c is a digit
+ * _isdigit - checks if c is a digit
  * Description - reurn 1 if digit and 0 otherwise
  * @c: integer to be tested
- * Return - 1 or 0
+ * return: 1 or 0
 */
 
 int _isdigit(int c)
 {
 return (c >= 48 && c <= 57);
-
 }
