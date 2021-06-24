2b06f43ba09f70881ff24c48d238978d8821cf46
Benson King'ori <bensonkingori96@gmail.com>
2021-06-24 10:53:44 -0700

    d

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 
 /**
  * _isdigit - checks if c is a digit
- * Description - reurn 1 if digit and 0 otherwise
  * @c: integer to be tested
- * return: 1 or 0
+ *
+ * Return: 1 or 0
 */
 
 int _isdigit(int c)
 {
-return (c >= 48 && c <= 57);
+return (c >= 48 && c <= 58);
 }
