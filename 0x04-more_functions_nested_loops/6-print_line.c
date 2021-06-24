66ab763c47af6f5b4168e0dfbdadc3cb74e300d2
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-06-24 20:48:55 +0400

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,14 @@
 
 void print_line(int n)
 {
-include "holberton.h"
-void print_line(int n)
-{
-int i = 0;
 
-while (i < n && n > 0)
-{
-_putchar('_');
-i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+
 }
