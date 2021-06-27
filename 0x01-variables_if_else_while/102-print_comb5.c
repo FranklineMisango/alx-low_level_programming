7e3e43e185a7e67f796d30ded04d3ea56ac9b70a
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-06-27 12:50:43 +0400

    k

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,37 +10,43 @@
 
 int main(void)
 {
-	int a;
-	int b;
-	int c = 0;
+int c;
+int d;
+int e;
+int f = 0;
 
-	while (c < 10)
+while (f < 10)
+{
+	e = 0;
+	while (e < 10)
 	{
-		b = 0;
-		while (b < 10)
+		d = 0;
+		while (d < 10)
 		{
-			a = 0;
-			while (a < 10)
+			c = 0;
+			while (c < 10)
 			{
-				if (a != b && b != c && c < b && b < a)
+				if (!(f == c && e == d))
 				{
+					putchar('0' + f);
+					putchar('0' + e);
+					putchar(' ');
+					putchar('0' + d);
 					putchar('0' + c);
-					putchar('0' + b);
-					putchar('0' + a);
-
-					if (a + b + c != 9 + 8 + 7)
+					if (!(f + e == 18 && c + d == 17 && d == 9))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-
-				a++;
+				c++;
 			}
-			b++;
+			d++;
 		}
-		c++;
+		e++;
 	}
-	putchar('\n');
-	return (0);
+	f++;
+}
+putchar('\n');
+return (0);
 }
