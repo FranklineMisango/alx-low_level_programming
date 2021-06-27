0f8c9f7f71acc99445432ec38d6dcd9c583f5637
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-06-27 12:44:54 +0400

    err

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,34 +10,37 @@
 
 int main(void)
 {
-int a;
-int b;
-int c = 0;
+	int a;
+	int b;
+	int c = 0;
 
-while (c < 10)
-{
-a = 1;
-b = 2;
-while (a < 10)
-{
-while (b < 10)
-if (c != a && c != b && a != b && c < a && c < b)
-{
-putchar('0' + c);
-putchar('0' + a);
-putchar('0' + b);
-if (a + c + b != 26)
-{
-putchar(',');
-putchar(' ');
-}
-}
-a++;
-}
-c++;
-}
-b++;
-}
-putchar('\n');
-return (0);
+	while (c < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			a = 0;
+			while (a < 10)
+			{
+				if (a != b && b != c && c < b && b < a)
+				{
+					putchar('0' + c);
+					putchar('0' + b);
+					putchar('0' + a);
+
+					if (a + b + c != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				a++;
+			}
+			b++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
