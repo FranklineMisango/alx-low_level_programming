34de30feeb2fa44b25b5ea61f3f5c9075ba17521
Benson King'ori <bensonkingori96@gmail.com>
2021-06-28 04:08:50 -0700

    e

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - Resets the value of the int pointer to 98
+ *@n: an integer
+ *return: 0
+ */
+
+void reset_to_98(int *n)
+{
+n = 98;
+
+_putchar(n);
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,2 @@
+/*update the value of an int pointer to 98*/
+void reset_to_98(int *n);
