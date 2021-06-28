abf8ebb362e64b983db18d462c3a48f4b58e2226
Benson King'ori <bensonkingori96@gmail.com>
2021-06-28 06:28:25 -0700

    we

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,11 @@
 /**
  * _strlen - Returns the value of a string
  * @s: string location
- * Return: void
+ * Return: 1
  */
 
 int _strlen(char *s)
 {
-_putchar(sizeof((*s));
+_putchar(sizeof((*s)));
+return (1);
 }
