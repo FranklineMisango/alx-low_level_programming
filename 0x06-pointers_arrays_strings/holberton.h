946b1ceaeec28461e09b91b8bbf50731a4bb6df6
Benson King'ori <bensonkingori96@gmail.com>
2021-06-30 04:07:43 -0700

    d

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,15 @@
+/*Concatenate two strings*/
+char *_strcat(char *dest, char *src);
+
+/*Concatenate n bytes*/
+char *_strncat(char *dest, char *src, int n);
+
+/*copies a string*/
+char *_strncpy(char *dest, char *src, int n);
+
+/*compares two strings*/
+int _strcmp(char *s1, char *s2);
+
+/*reverse integer array*/
+void reverse_array(int *a, int *n);
+
