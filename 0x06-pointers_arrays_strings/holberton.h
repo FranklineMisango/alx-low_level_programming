9496768707f0457e75ac8576f1e7325c788449b5
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-07-01 22:23:56 +0400

    fd

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,33 +1,15 @@
-/*Input data*/
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
-
-/*Concatenate two strings*/
 char *_strcat(char *dest, char *src);
-
-/*Concatenate n bytes*/
 char *_strncat(char *dest, char *src, int n);
-
-/*copies a string*/
 char *_strncpy(char *dest, char *src, int n);
-
-/*compares two strings*/
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *rot13(char *str);
 
-/*reverse integer array*/
-void reverse_array(int *a, int *n);
-
-/*Changes all lowercase letters to uppercase*/
-char *string_toupper(char *);
-
-/*Capitalize all words of a string*/
-char *cap_string(char *);
-
-/*leet question*/
-char *leet(char *);
-
-/*rot whatever*/
-char *rot13(char *);
-
-void print_number(int n);
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif /* HOLBERTON_H */
