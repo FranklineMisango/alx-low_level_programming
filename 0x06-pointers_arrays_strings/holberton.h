69da12af122312873a4ffe74ddcaedfd0a3fd7e8
Benson King'ori <bensonkingori96@gmail.com>
2021-07-01 09:00:25 -0700

    w

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -13,3 +13,8 @@ int _strcmp(char *s1, char *s2);
 /*reverse integer array*/
 void reverse_array(int *a, int *n);
 
+/*Changes all lowercase letters to uppercase*/
+char *string_toupper(char *);
+
+/*Capitalize all words of a string*/
+char *cap_string(char *);
