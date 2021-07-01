46dd1a87f94159570f214a82483c7c1abb253040
Benson King'ori <bensonkingori96@gmail.com>
2021-07-01 09:33:13 -0700

    w

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * *_strncpy - find the length of a string
  * @dest: pointer to the string
@@ -13,10 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
+dest[i] = src[i];
 
 for ( ; i < n; i++)
-	dest[i] = '\0';
-
+dest[i] = '\0';
+ 
 return (dest);
 }
