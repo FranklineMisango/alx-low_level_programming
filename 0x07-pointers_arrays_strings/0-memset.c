0d1278969b27859b35c7eddf18da588a6a95aff0
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 08:57:25 -0700

    e

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-while (i <= n)
+  unsigned int i;
+  for (i = 0; n > 0; i++, n--)
 {
 s[i] = b;
-i++;
 }
 return (s);
 }
