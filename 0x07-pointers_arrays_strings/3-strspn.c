a4a75a3d10fb2d0f1458836d896b494f948d3f8d
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 09:42:51 -0700

    2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,18 @@
 #include "holberton.h"
 
 /**
- * function -
- * @parameter:
- * Return - 
+ * _strspn -gets the length of a prefix substring
+ * @s: the string
+ * @accept: another string
+ * Return: number of bytes of accept in s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0;
+while (s[i] == accept[i])
+{
+i++;
+return (i);
+}
+}
