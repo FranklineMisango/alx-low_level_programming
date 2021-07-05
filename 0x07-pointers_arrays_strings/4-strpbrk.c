3df0bf68c4a45b5343719bad10040aed6da47870
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 09:51:58 -0700

    s

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
- * function -
- * @parameter:
- * Return - 
+ * _strpbrk - searches a string for any of a set of bytes
+ * @S: source string
+ * @accept: second string
+ * Return: s or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int index;
+
+while (*s)
+{
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+{
+return (s);
+}
+}
+s++;
+}
+return ('\0');
+}
