6a956f815dedd7e61c24e8dbf7c921859d227c91
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 09:17:49 -0700

    d

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,24 @@
 #include "holberton.h"
 
 /**
- * function -
- * @parameter:
- * Return - 
+ * _strchr - locates a character in a string
+ * @s: the source string
+ * @c: character to be located
+ * Return: pointer to c
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; i != '\0'; i++)
+{
+if s[i] = c
+{
+return (c);
+}
+else
+{
+return (NULL);
+}
+}
+}
