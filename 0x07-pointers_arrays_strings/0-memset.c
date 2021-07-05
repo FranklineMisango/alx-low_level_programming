45c48294a79f133bd514dc4bfc525f1081aa4262
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 08:55:01 -0700

    w

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * *_memset -fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: pointer to the memory to be filled
  * @b: constant byte
  * @n: number of bytes of s to be filled by b
- * Return - pointer to s
+ * Return: pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
