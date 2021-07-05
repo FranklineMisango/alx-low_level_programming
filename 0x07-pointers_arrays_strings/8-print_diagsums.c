8725a06c724a5c7dfe5f16dc083287311f22caae
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 10:13:19 -0700

    w

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
- * function -
- * @parameter:
- * Return - 
+ * print_diagsums - prints the sum of 2 square matrix of integers
+ * @a: first integer
+ * @size: second integer
+ * Return: the sum
+ */
+
+void print_diagsums(int *a, int size)
+{
+int x, sum1 = 0, sum2 = 0;
+
+for (x = 0; x < size; x++)
+{
+sum1 += a[x];
+a += size;
+}
+a -= size;
+for (x = 0; x < size; x++)
+{
+sum2 += a[x];
+a -= size;
+}
+printf("%d, %d\n", sum1, sum2);
+}
