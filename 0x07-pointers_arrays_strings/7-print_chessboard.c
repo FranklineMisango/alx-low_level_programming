30c9b98db483301139e4951f2a723d8a308caf08
Benson King'ori <bensonkingori96@gmail.com>
2021-07-05 10:07:32 -0700

    w

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
- * function -
- * @parameter:
- * Return - 
+ * print_chessboard - prints the chessboard
+ * @a: an integer for the number of pieces
+ * Return: chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int x, y;
+
+for (x = 0; x < 8; x++)
+{
+for (y = 0; y < 8; y++)
+{
+_putchar(a[x][y]);
+}
+_putchar('\n');
+}
+}
