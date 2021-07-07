150f41a3c3b044cfed8c4ba281fe6df9e00c3af9
Benson King'ori <bensonkingori96@gmail.com>
2021-07-07 13:49:05 -0700

    e

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,11 +8,14 @@
 
 int _strlen_recursion(char *s)
 {
-int i = 0;
-if (*s > '\0')
+int length;
+
+length = 0;
+
+if (*s)
 {
-i += _strlen_recursion(s + 1) + 1;
+length++;
+length = length + _strlen_recursion(s + 1);
 }
-return (i);
-}
+return (length);
 }
