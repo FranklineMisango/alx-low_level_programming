d64c13bbd7f426b6ad27783e8e574a303d95bf0b
Benson King'ori <bensonkingori96@gmail.com>
2021-07-07 06:19:19 -0700

    w

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to be output
+ * Return:0 or string
+ */
+
+void _puts_recursion(char *s)
+{
+int i = 0;
+char str;
+
+if s[i] = '\0'
+{
+return ();
+}
+str = s[i] + s[i + 1]
+i++;
+_putchar(str);
+_putchar(\n);
+}
