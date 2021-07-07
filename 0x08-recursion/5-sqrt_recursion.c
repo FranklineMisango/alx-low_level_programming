2ce869da54404b750702ba8e09f36f26bba82c85
Benson King'ori <bensonkingori96@gmail.com>
2021-07-07 13:37:17 -0700

    e

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,31 @@
 #include "holberton.h"
 
 /**
- * function - 
- * @parameter: 
- * Return:
+ * _sqrt_recursion - find natural square root
+ * @n: int
+ * Return: int
  */
 
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int
+ */
+
+int square(int n, int val)
+{
+
+if (val * val == n)
+return (val);
+else if (val * val < n)
+return  (square(n, val + 1));
+else
+return (-1);
+
+}
