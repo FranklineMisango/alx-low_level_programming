8c32d3fb649a866202a9bfa0cbc750a01650a8e8
Benson King'ori <bensonkingori96@gmail.com>
2021-07-07 07:02:08 -0700

    f

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * function - 
+ * _print_rev_recursion - prints a string in reverse 
  * @parameter: 
  * Return:
  */
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,19 @@
 #include "holberton.h"
 
 /**
- * function - 
- * @parameter: 
- * Return:
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: 0 or i
  */
 
+int _strlen_recursion(char *s)
+{
+int i = 0;
+if (s[i] == '\0')
+return (0);
+{
+i = i + 1;
+i++;
+return (i);
+}
+}
