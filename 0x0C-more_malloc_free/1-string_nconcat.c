6f9e69c79db69dfeb0518aec92fc1d0b57c97157
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-07-15 21:51:14 +0400

    ehioqf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
+
 /**
- * *string_nconcat - concatenates two strings 
+ * _strlen - find length of a string
  * @s: string
  * Return: int
-*/
+ */
 
 unsigned int _strlen(char *s)
 {
 unsigned int size = 0;
-for (; s[size] != '\0'; size++);
+for (; s[size] != '\0'; size++)
+;
 return (size);
 }
 
@@ -22,28 +24,27 @@ return (size);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 unsigned int i, j;
 char *m;
 
 if (s1 == NULL)
-s1 = "";
+	s1 = "";
 if (s2 == NULL)
-s2 = "";
+	s2 = "";
 
 if (n < _strlen(s2))
-m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+	m = malloc(_strlen(s1) + n * sizeof(char) + 1);
 else
-m = malloc(_strlen(s1) + _strlen(s2) + 1);
+	m = malloc(_strlen(s1) + _strlen(s2) + 1);
 
 if (m == 0)
-return (NULL);
+	return (NULL);
 
 for (i = 0; s1[i] != '\0'; i++)
-m[i] = s1[i];
+	m[i] = s1[i];
 
 for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-m[i] = s2[j];
+	m[i] = s2[j];
 
 m[i] = '\0';
 
