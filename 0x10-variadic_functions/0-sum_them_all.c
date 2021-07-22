2eea35ce9e87c7965e3cfcd09bde0d66f13f7299
Benson King'ori <bensonkingori96@gmail.com>
2021-07-22 10:29:32 -0700

    s

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums up the variadic arguments
+ * @n:number of variadic arguments
+ * Return: sum of variadic parameters or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+ 
+va_list ap;
+int i, sum;
+
+va_start (ap, n);
+
+sum = 0;
+for (i = 0; i < n; i++)
+sum += va_arg (ap, int);
+
+va_end (ap);
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+<p>Here comes the variations<\p>
