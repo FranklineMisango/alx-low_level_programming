21a16476711b92a356053a6e19e9a84e031bbad1
Benson King'ori <bensonkingori96@gmail.com>
2021-08-06 01:31:27 -0700

    d

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,21 +17,21 @@ int i = 0;
 int c;
 unsigned int len;
 
-  if (b == NULL)
-    return 0;
+if (b == NULL)
+return (0);
 
 len = strlen(b);
 
-  for (c = len-1; c >= 0; c--)
- {
-  if (b[c] != '0' && b[c] != '1')
-      return (0);
- }
-  while (b[i] == '0' || b[i] == '1')
-  {  
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
+for (c = len - 1; c >= 0; c--)
+{
+if (b[c] != '0' && b[c] != '1')
+return (0);
+}
+while (b[i] == '0' || b[i] == '1')
+{
+val <<= 1;
+val += b[i] - '0';
+i++;
+}
+return (val);
 }
