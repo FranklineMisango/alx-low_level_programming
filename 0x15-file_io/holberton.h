ac2f2740c667c21a469f78cf4c496c690db6eea5
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-08-09 20:07:08 +0400

    bjksa

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,9 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+
 #include <stdlib.h>
-#include <sys/types.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include <unistd.h>
 
@@ -12,4 +15,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* HOLBERTON_H */
