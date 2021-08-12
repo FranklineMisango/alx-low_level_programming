9b17043a4e3d6a7db6991152250fdb83c67fa434
Benson King'ori <bensonkingori96@gmail.com>
2021-08-12 04:52:45 -0700

    d

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,33 +12,33 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, read_fun, write_fun;
-	char *tmp;
+int fd, read_fun, write_fun;
+char *tmp;
 
-	if (filename == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	tmp = malloc(sizeof(char) * letters);
+tmp = malloc(sizeof(char) * letters);
 
-	if (tmp == NULL)
-		return (0);
+if (tmp == NULL)
+return (0);
 
-	fd = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
 
-	if (fd == -1)
-		return (0);
+if (fd == -1)
+return (0);
 
-	read_fun = read(fd, tmp, letters);
+read_fun = read(fd, tmp, letters);
 
-	if (read_fun == -1)
-		return (0);
+if (read_fun == -1)
+return (0);
 
-	write_fun =  write(STDOUT_FILENO, tmp, read_fun);
+write_fun =  write(STDOUT_FILENO, tmp, read_fun);
 
-	if (write_fun == -1)
-		return (0);
-	close(fd);
+if (write_fun == -1)
+return (0);
+close(fd);
 
-	free(tmp);
-	return (write_fun);
+free(tmp);
+return (write_fun);
 }
