d0a836c68ab239aeb811e0e7c222bd38e6d98764
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-19 00:20:16 +0400

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-
 #include <stdio.h>
+
 /**
  * main - prints to string
  *
@@ -7,6 +7,7 @@
  *
  * Return: 1
  */
+
 int main(void)
 {
 char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
