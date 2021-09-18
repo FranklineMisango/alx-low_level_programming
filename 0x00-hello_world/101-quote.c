f50bb8299f9242c6a0f6ff85949de517ad0953ae
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-09-18 23:42:32 +0400

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(2, ch, sizeof(ch));
-fwrite("[stderr] : No string argument provided!\n", 40, 1, stderr);
+fwrite(stderr, "[stderr] : No string argument provided!\n");
 
 return (1);
 }
