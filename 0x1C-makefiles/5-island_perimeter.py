dc0ef0be354c7592e3f7d2a0ed96865f0231efc5
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-11-24 22:03:53 +0400

    Add files via upload

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pverthon3
+
+"""A module to calculate perimeter of an island"""
+
+def island_perimeter(grid):
+    """The function itself"""
+
+    count = 0
+    for ver, row in enumerate(grid):
+        for hor, cell in enumerate(row):
+            if cell == 1:
+                if ver == 0 or grid[ver - 1][hor] == 0:
+                    count += 1
+                if ver == len(grid) - 1 or grid[ver + 1][hor] == 0:
+                    count += 1
+                if hor == 0 or grid[ver][hor - 1] == 0:
+                    count += 1
+                if hor == len(row) - 1 or grid[ver][hor + 1] == 0:
+                    count += 1
+            else:
+                pass
+    return count
